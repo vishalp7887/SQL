1ae03c064169f61643e86d8c8828648c81a7c55e
vishalp7887 <vp385958@gmail.com>
2022-11-11 14:15:37 +0530

    aggregate function

---
+++ practice mysql/aggregate function.sql
@@ -0,0 +1,13 @@
+USE demo;
+
+SELECT * FROM sample;
+
+SELECT SUM(education) AS sum FROM sample;
+
+SELECT COUNT(education) AS COUNT FROM sample;
+
+SELECT AVG(education) AS average FROM sample;
+
+SELECT MIN(education) AS MINIMUM FROM sample;
+
+SELECT MAX(education) AS MAXIMUM FROM sample;
