783c08d2a65b2356489e0b52fa5a5ee2990918a9
vishalp7887 <vp385958@gmail.com>
2022-11-11 14:03:20 +0530

    practice

---
+++ practice mysql/practice.sql
@@ -0,0 +1,22 @@
+USE demo;
+
+SELECT * FROM ssd3;
+
+SELECT * FROM naturalgas;
+
+SELECT SUM(price) AS total FROM naturalgas;
+
+SELECT AVG(price) AS average FROM naturalgas;
+
+SELECT COUNT(price) AS count FROM naturalgas;
+
+SELECT MIN(price) AS minimum FROM naturalgas;
+
+SELECT MAX(price) AS maximum FROM naturalgas;
+
+SELECT * FROM naturalgas;
+
+SELECT price FROM naturalgas;
+
+SELECT * FROM naturalgas WHERE myunknowncolumn = 8;
+
